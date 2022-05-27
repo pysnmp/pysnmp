e2926c530d08590744e457bf8da19f74bebee812
omrozowicz-splunk <omrozowicz@splunk.com>
2022-05-27 17:19:55 +0200

    fix: add automatic yes to the poetry command

--- .github/workflows/sr-release.sh
+++ .github/workflows/sr-release.sh
@@ -3,4 +3,4 @@
 set -eE
 set -v
 echo pypy user=${PYPI_USERNAME}
-poetry publish --build --username ${PYPI_USERNAME} --password ${PYPI_TOKEN}
+yes | poetry publish --build --username ${PYPI_USERNAME} --password ${PYPI_TOKEN}
