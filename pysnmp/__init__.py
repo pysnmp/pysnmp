96e843db4cecabf17afa5aede4b1857805dbd251
omrozowicz-splunk <omrozowicz@splunk.com>
2022-11-08 10:52:57 +0100

    fix: update init file

--- pysnmp/__init__.py
+++ pysnmp/__init__.py
@@ -1,5 +1,7 @@
 # http://www.python.org/dev/peps/pep-0396/
 __version__ = '5.0.19'
+# another variable is required to prevent semantic release from updating version in more than one place
+main_version = __version__
 # backward compatibility
 # for beta versions, integer casting throws an exception, so string part must be cut off
 if 'beta' in __version__:
