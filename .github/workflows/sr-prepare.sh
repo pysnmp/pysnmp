842a7dc817cb55b96ef78fc73f914dd4d94e8e00
Ryan Faircloth <ryan@dss-i.com>
2023-11-28 09:52:17 -0500

    ci: publish after commit and use new secure approach

--- .github/workflows/sr-prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eE
-set -v
-# shellcheck disable=SC1091,SC2086
-poetry build
--- .github/workflows/sr-release.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -eE
-set -v
-echo pypy user=${PYPI_USERNAME}
-yes | poetry publish -vv --build --username ${PYPI_USERNAME} --password ${PYPI_TOKEN}
